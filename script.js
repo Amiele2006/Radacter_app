8d09b183b87269fd94a09fc88fd527a1ade3735e
Funmi <emily.yahoo.co.uk@gmail.com>
2025-02-12 18:38:58 +0100

    fixed: space error on textarea input

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function replaceKeywords(text, keywords, replacementChar) {
+  var words = text.split(' ');
+  var keywordSet = new Set(keywords);
+  for (var i = 0; i < words.length; i++) {
+    if (keywordSet.has(words[i])) {
+      words[i] = replacementChar.repeat(words[i].length);
+    }
+  }
+  return words.join(' ');
+}
+
 document.getElementById('replace-form').addEventListener('submit', function(event) {
   event.preventDefault();
   var text = document.getElementById('text').value;
@@ -7,23 +18,13 @@ document.getElementById('replace-form').addEventListener('submit', function(even
   document.getElementById('output-text').innerText = output;
 });
 
-function replaceKeywords(text, keywords, replacementChar) {
-  var words = text.split(' ');
-  for (var i = 0; i < words.length; i++) {
-    if (keywords.includes(words[i])) {
-      words[i] = replacementChar.repeat(words[i].length);
-    }
-  }
-  return words.join(' ');
-}
-
 //making a function to reset the form if clicked on 
 function resetForm() {
   document.getElementById('replace-form').reset();
   document.getElementById('output-text').innerText = ""; // Clear the output text
 }
 
-//adding a click event listener to the reset button
-document.getElementById('reset-button').addEventListener('click', function() {
+document.getElementById('reset-button').addEventListener('click', function(event) {
+  event.preventDefault();
   resetForm();
-});
+})
